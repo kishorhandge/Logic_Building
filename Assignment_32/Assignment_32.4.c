fe6638640c618040db4967d421b0363b04f8b58d
Kishor Handge <kishorhandge29@gmail.com>
2025-11-25 23:37:23 +0530

    Initial Upload

---
+++ Assignment_32/Assignment_32.4.c
@@ -0,0 +1,57 @@
+/*
+    iRow : 4
+    iCol : 4
+    
+
+1       2       3       4
+1       2               4
+1               3       4
+1       2       3       4
+ 
+
+*/
+#include <stdio.h>
+
+void Pattern(int iRow, int iCol)
+{
+    int i = 0, j = 0;
+
+    for (i = iRow; i >= 1;  i--)
+    {
+
+        for (j = 1; j <=iCol ; j++)
+        {
+            if((i == 1) ||(j == iCol) || (i == 1) ||(j == 1) || (i == j))
+                {
+                    printf("*\t"); 
+                  
+                }
+                else if(i>j)
+                {
+                    printf("#\t");  
+                }
+                else
+                {
+                    printf("$\t");
+                }
+                
+             
+        }
+
+        printf("\n");
+    }
+}
+
+int main()
+{
+    int iValue1 = 0, iValue2 = 0;
+
+    printf("Enter the number of rows :\n");
+    scanf("%d", &iValue1);
+
+    printf("Enter the number of columns :\n");
+    scanf("%d", &iValue2);
+
+    Pattern(iValue1, iValue2);
+    return 0;
+}
